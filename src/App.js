cca270011e58d2e2724e757c5663d0c0ad66c781
Meixuan Wu <mwu@LABA.PSWAS.LOCAL>
2023-05-09 09:44:18 -0700

    task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './componets/Product';
+import { useState } from 'react';
 
-function App() {
+
+function ProductList() {
+  const [products,setProducts]= useState([ {
+    name: "Smartwatch",
+    price: 150,
+    description: "A wearable device that can receive notifications, track fitness, and more.",
+    img: "https://via.placeholder.com/200x200?text=Smartwatch"
+  },
+  {
+    name: "Wireless Earbuds",
+    price: 80,
+    description: "Earbuds that connect to your phone or tablet via Bluetooth, with noise cancelling and long battery life.",
+    img: "https://via.placeholder.com/200x200?text=Wireless+Earbuds"
+  },
+  {
+    name: "Smart Thermostat",
+    price: 200,
+    description: "A device that can control the temperature of your home and learn your preferences over time.",
+    img: "https://via.placeholder.com/200x200?text=Smart+Thermostat"
+  },
+  {
+    name: "Wireless Charger",
+    price: 30,
+    description: "A charging pad that can wirelessly charge your phone or other devices that support wireless charging.",
+    img: "https://via.placeholder.com/200x200?text=Wireless+Charger"
+  },
+  {
+    name: "Virtual Reality Headset",
+    price: 400,
+    description: "A device that provides a fully immersive experience of a virtual world.",
+    img: "https://via.placeholder.com/200x200?text=Virtual+Reality+Headset"
+  },
+  {
+    name: "Smart Speaker",
+    price: 100,
+    description: "A voice-activated speaker that can play music, control your smart home devices, and answer questions.",
+    img: "https://via.placeholder.com/200x200?text=Smart+Speaker"
+  },
+  {
+    name: "Laptop",
+    price: 800,
+    description: "A portable computer that allows you to work or play on the go.",
+    img: "https://via.placeholder.com/200x200?text=Laptop"
+  },
+  {
+    name: "Drone",
+    price: 500,
+    description: "A flying device that can capture aerial footage or perform tasks.",
+    img: "https://via.placeholder.com/200x200?text=Drone"
+  },
+  {
+    name: "Action Camera",
+    price: 250,
+    description: "A compact camera that can capture high-quality footage of your adventures.",
+    img: "https://via.placeholder.com/200x200?text=Action+Camera"
+  },
+  {
+    name: "Gaming Console",
+    price: 400,
+    description: "A device that allows you to play video games on your TV or monitor.",
+    img: "https://via.placeholder.com/200x200?text=Gaming+Console"
+  }
+
+])
+  const productsJxs = products.map((product, index) => (
+     <Product key={index} name={product.name} price={product.price} />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Products</h2>
+      <ul>{productsJxs}</ul>
     </div>
   );
 }
 
-export default App;
+
+
+export default ProductList;
---
+++ src/componets/Product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Products = (props) => {
+
+
+
+  return (
+    <div>
+      {props.name}
+      {props.price}
+    </div>
+  )
+}
+
+export default Products
+
